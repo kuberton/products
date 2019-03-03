9bf905ab4f4ed6b1fa35ff7029cdae9e5f452357
nikolaiev <Vladyslav.Nikolaiev@gmail.com>
2019-03-03 13:52:41 +0200

    updated cloudbuild.yaml

--- src/main/java/com/kuber/products/repository/ProductsRepository.java
+++ src/main/java/com/kuber/products/repository/ProductsRepository.java
@@ -4,6 +4,6 @@ import com.kuber.products.dto.Product;
 import org.springframework.cloud.gcp.data.datastore.repository.DatastoreRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public interface ProductsRepository extends DatastoreRepository<Product, Long> {
 }
